9f7c2f04dc064302998e8ca703dddaef7ff0983c
ValenButtignol <valentinbuttignol@gmail.com>
2023-06-07 15:43:58 -0300

    changes done

--- derived_functions.py
+++ derived_functions.py
@@ -1,6 +1,5 @@
-from virus_propagation import beta, v, h, s0, i0
 
-def s1(t):
+""" def s1(t):
     print(beta)
     print(h)
     print(s0)
@@ -11,27 +10,64 @@ def i1(t):
     return beta * q(t, s0, h, s1) * q(t, i0, h, i1) - v + q(t, i0, h, i1)
 
 def r1(t):
-    return v * q(t, i0, h, i1)
+    return v * q(t, i0, h, i1) """
 
 """ def r(t, h, beta, v, s0, i0, r0):
-    if r0 == 0:
+    if t <= 0:
         return r0
     else:
-        return r(t-h, h, beta, v s0, i0, r0) + h * v * i(t-h, h, beta, v, s0, i0, r0)
+        return r(t-h, h, beta, v, s0, i0, r0) + h * v * i(t-h, h, beta, v, s0, i0, r0)
 
 def i(t, h, beta, v, s0, i0, r0):
-    if i0 == 0:
+    if t <= 0:
         return i0
     else:
-        return i(t-h, h, beta, v s0, i0, r0) + h * beta * s(t-h, h, beta, v, s0, i0, r0) - v * i(t-h, h, beta, v s0, i0, r0)
+        return i(t-h, h, beta, v, s0, i0, r0) + h * beta * s(t-h, h, beta, v, s0, i0, r0) - v * i(t-h, h, beta, v, s0, i0, r0)
         
 
 def s(t, h, beta, v, s0, i0, r0):
-    if s0 == 0:
+    if t <= 0:
         return s0
     else:
-        return s(t-h, h, beta, v, s0, i0, r0) + h * (-1 * beta) * s(t-h, h, beta, v, s0, i0, r0) * i(t-h, h, beta, v s0, i0, r0) """
-        
+        return s(t-h, h, beta, v, s0, i0, r0) + h * (-1 * beta) * s(t-h, h, beta, v, s0, i0, r0) * i(t-h, h, beta, v, s0, i0, r0) 
+  """
+
+# Diccionario para almacenar los resultados memoizados
+memo_retired = {}
+
+def retired_deritative(t, h, beta, v, s0, i0, r0):
+    if t <= 0:
+        return r0
+    elif t in memo_retired:  # Comprobar si el resultado ya está memoizado
+        return memo_retired[t]
+    else:
+        result = retired_deritative(t-h, h, beta, v, s0, i0, r0) + h * v * infected_deritative(t-h, h, beta, v, s0, i0, r0)
+        memo_retired[t] = result  # Almacenar el resultado memoizado
+        return result
+
+memo_infected = {}
+
+def infected_deritative(t, h, beta, v, s0, i0, r0):
+    if t <= 0:
+        return i0
+    elif t in memo_infected:
+        return memo_infected[t]
+    else:
+        result = infected_deritative(t-h, h, beta, v, s0, i0, r0) + h * beta * suceptible_deritative(t-h, h, beta, v, s0, i0, r0) - v * infected_deritative(t-h, h, beta, v, s0, i0, r0)
+        memo_infected[t] = result
+        return result
+
+memo_suceptible = {}
+
+def suceptible_deritative(t, h, beta, v, s0, i0, r0):
+    if t <= 0:
+        return s0
+    elif t in memo_suceptible:
+        return memo_suceptible[t]
+    else:
+        result = suceptible_deritative(t-h, h, beta, v, s0, i0, r0) + h * (-beta) * suceptible_deritative(t-h, h, beta, v, s0, i0, r0) * infected_deritative(t-h, h, beta, v, s0, i0, r0)
+        memo_suceptible[t] = result
+        return result
 
 def y(t, h):
     if t == 0:
@@ -42,7 +78,7 @@ def y(t, h):
 
 # Generic euler function
 def q(t:float, base_case:float, h:float, derived_function):
-    if t == 0:
+    if t <= 0:
         return base_case
     else:
         return q(t-h, base_case, h, derived_function) + h * derived_function(t-h)
--- virus_propagation.py
+++ virus_propagation.py
@@ -1,37 +1,72 @@
-from parser import parse_input
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+from derived_functions import q, suceptible_deritative, retired_deritative, infected_deritative
+from parser_1 import parse_input
 
 def hours_to_days(t):
-    return t/24
+    return t*24
 
- 
-""" def fun():
-    import matplotlib
+def make_plot(time, h, beta, v, s0, i0, r0):
     matplotlib.use('TkAgg')
-    from matplotlib import pyplot as plt """
+    x = []
+    j = 0
+    while (j < time):
+        x.append(j)
+        j+=h
+
+    y0 = [suceptible_deritative(j, h, beta, v, s0, i0, r0) for j in x]
+    y1 = [infected_deritative(j, h, beta, v, s0, i0, r0) for j in x]
+    y2 = [retired_deritative(j, h, beta, v, s0, i0, r0) for j in x]
+   
+    plt.plot(x, y0, linewidth=2.0, color='blue', label='Susceptible People')
+    plt.plot(x, y1, linewidth=2.0, color='red', label='Infected People')
+    plt.plot(x, y2, linewidth=2.0, color='green', label='Retired People')
+    plt.title('Virus Propagation')
+    plt.legend()
+    plt.show()
+
+    """ ax0.plot(x, y0, linewidth=2.0)
+    ax1.plot(x, y1, linewidth=2.0)
+    ax2.plot(x, y2, linewidth=2.0)
+    
+    ax0.legend()
+    ax0.set_xlabel('Time (hours)')
+    ax0.set_ylabel('Susceptible People')
+    
+    ax1.legend()
+    ax1.set_xlabel('Time (hours)')
+    ax1.set_ylabel('Infected People')
+    
+    ax2.legend()
+    ax2.set_xlabel('Time (hours)')
+    ax2.set_ylabel('Retired People')
 
 
-t = 0
-beta = 0
-v = 0
-s0 = 0
-i0 = 0
-r0 = 0
-t = 0
-h = 0
+    ax0.set_xticks(np.arange(0,700, 24))
+    ax0.set_yticks(np.arange(0,1000, 50))
+    ax1.set_xticks(np.arange(0,700, 24))
+    ax1.set_yticks(np.arange(0,1000, 50))
+    ax2.set_xticks(np.arange(0,700, 24))
+    ax2.set_yticks(np.arange(0,1000, 50))
+ """
+
 if __name__ == "__main__":
-    from derived_functions import q, s1
 
     args = parse_input()
 
-    t = args.time
-    beta = args.beta
-    v = args.v
-    s0 = args.s0
-    i0 = args.i0
-    r0 = args.r0
+    t = float(args.time)
+    h = float(args.delta)
+    beta = float(args.beta)
+    v = float(args.v)
+    s0 = float(args.s0)
+    i0 = float(args.i0)
+    r0 = float(args.r0)
 
-    i = 0
-    h = 0.5
-    print(q(2, 1, h, s1))
-    while i <= 3.0:
-        i += 0.5
+    print(suceptible_deritative(t, h, beta, v, s0, i0, r0))
+    print(infected_deritative(t, h, beta, v, s0, i0, r0))
+    print(retired_deritative(t, h, beta, v, s0, i0, r0))
+
+
+    make_plot(hours_to_days(t), h, beta, v, s0, i0, r0)
